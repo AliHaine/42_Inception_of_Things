c07dd9b4120f07964e2946f516d04ef65efa014e
AliHaine <alitr57120@gmail.com>
2025-06-22 05:03:06 +0200

    Update part 2

---
+++ p2/scripts/controller.sh
@@ -0,0 +1,4 @@
+curl -sfL https://get.k3s.io | sh -
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
