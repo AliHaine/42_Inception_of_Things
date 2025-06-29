dfc17dfbc10518e71542ed89cd2f8acb23a041b7
AliHaine <alitr57120@gmail.com>
2025-06-29 08:50:15 +0200

    Manually create namespace, add some check and verification steps, run argocd on port 8081

--- p3/scripts/run.sh
+++ p3/scripts/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 #Create cluster and namespace
+
+if k3d cluster get maincluster >/dev/null 2>&1; then
+    k3d cluster delete maincluster
+fi
+
 k3d cluster create maincluster -p "8080:80@loadbalancer"
 kubectl create namespace argocd
+kubectl create namespace dev
 
 #Install the argocd application
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -10,25 +16,32 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 echo "Waiting for ArgoCD components to be ready..."
 kubectl wait --namespace argocd --for=condition=Available deploy --all --timeout=380s
 
-echo "ArgoCD Admin Password:"
+echo "\nArgoCD Admin Password:\n"
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
-echo ""
+echo "\n"
 
 #Wait for port stabilization
 sleep 5
 
 # Port-forward in background
 echo "Starting port-forward to ArgoCD GUI..."
-kubectl port-forward svc/argocd-server -n argocd 9090:80 > /dev/null &
+kubectl port-forward svc/argocd-server -n argocd 8081:80 >/dev/null 2>&1 &
 
 #Wait for port stabilization
 sleep 5
 
 #Apply my argo manifest
-kubectl apply -n argocd -f configs/argo.yml
+echo "Applying argocd custom manifest"
+kubectl apply -n argocd -f argo.yml
+
+until kubectl get svc will -n dev >/dev/null 2>&1; do
+    sleep 1
+done
 
 # Port-forward in background for will app
 echo "Starting port-forward to will app..."
-kubectl port-forward svc/will -n dev 8888:80 > /dev/null &
+kubectl port-forward svc/will -n dev 8888:80 >/dev/null &
 
-echo "Setup complete. You can access ArgoCD at https://localhost:8080"
+echo "Setup complete."
+echo "You can access ArgoCD GUI at https://localhost:8081"
+echo "You can access will app at https://localhost:8888"
