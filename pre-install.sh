7686fd67e1427a5a11f1d271e2dde7ced988cbfb
AliHaine <alitr57120@gmail.com>
2025-07-23 11:10:49 +0200

    pre-install script total rewrite.

--- pre-install.sh
+++ pre-install.sh
@@ -1,24 +1,24 @@
+#!/bin/zsh
+
+rm -rf ~/bin
+
+mkdir -p ~/bin
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
+
 ##vagrant
-cd ~
-wget https://releases.hashicorp.com/vagrant/2.4.1/vagrant_2.4.1_linux_amd64.zip
-unzip vagrant_2.4.1_linux_amd64.zip -d ~/vagrant
-echo 'export PATH="$HOME/vagrant:$PATH"' >> ~/.zshrc
-source ~/.zshrc
-vagrant --version
+wget https://releases.hashicorp.com/vagrant/2.4.7/vagrant_2.4.7_linux_amd64.zip
+unzip vagrant_2.4.7_linux_amd64.zip -d ~/bin
+sleep 1
+rm -rf ./vagrant_2.4.7_linux_amd64.zip
 
 ##k3d
-mkdir -p ~/bin
-export PATH="$HOME/bin:$PATH"
-source ~/.bashrc
-curl -Lo ~/bin/k3d https://github.com/k3d-io/k3d/releases/latest/download/k3d-linux-amd64
+curl -Lo ~/bin/k3d https://github.com/k3d-io/k3d/releases/download/v5.7.0/k3d-linux-amd64
 chmod +x ~/bin/k3d
-k3d version
 
 ##kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-mkdir -p ~/bin
-mv ./kubectl ~/bin/kubectl
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o ~/bin/kubectl
+chmod +x ~/bin/kubectl
+
+vagrant --version
+k3d version
 kubectl version --client
