232324185f93099bef7e19d310acc396f40b51d7
AliHaine <alitr57120@gmail.com>
2025-07-24 14:22:25 +0200

    Add makefile for p2. Global refactor and setup for correction. internal ip fix in p2. p3 still not running on school computer

--- p2/scripts/controller.sh
+++ p2/scripts/controller.sh
@@ -1,4 +1,4 @@
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --write-kubeconfig-mode=644" sh -
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
